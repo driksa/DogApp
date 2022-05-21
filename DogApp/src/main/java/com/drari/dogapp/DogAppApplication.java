174340d3c2e79a726831ceebf59f26d3ef2d17d4
Rasam <h.oussama297@gmail.com>
2022-05-21 13:54:47 +0200

    first commit

---
+++ DogApp/src/main/java/com/drari/dogapp/DogAppApplication.java
@@ -0,0 +1,13 @@
+package com.drari.dogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogAppApplication.class, args);
+	}
+
+}
